d07309d466d95a5be3b1c70be24c9bbba1b1dba1
Topan-Roy <roytopan734@gmail.com>
2025-08-12 23:34:55 +0600

    added update footer

--- src/Shared/Footer.jsx
+++ src/Shared/Footer.jsx
@@ -2,11 +2,10 @@ import React from 'react';
 import { FaFacebook, FaLinkedin, FaYoutube } from 'react-icons/fa';
 import { FaSquareInstagram } from 'react-icons/fa6';
 
-
 const Footer = () => {
   return (
     <footer className="bg-gray-100 dark:bg-gray-900 text-gray-700 dark:text-gray-300 border-t dark:border-gray-700 pt-10 pb-6 px-4 md:px-20 mt-5">
-      <div className="max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
+      <div className="max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
 
         {/* Branding */}
         <div>
@@ -15,10 +14,10 @@ const Footer = () => {
             Your all-in-one platform for collaborative learning, assignments, and academic growth.
           </p>
           <div className="flex gap-4 mt-4 text-xl">
-             <a href="https://www.facebook.com/TopanRoy41105/" ><FaFacebook  color='blue'/></a>
-        <a href="https://www.instagram.com/topan_roy41105/?igsh=dWdpYmUxcDNuZnhz#" ><FaSquareInstagram  color='red'/></a>
-        <a href="https://www.linkedin.com/in/topanroy/" ><FaLinkedin color='blue'/></a>
-        <a href="https://www.youtube.com/@Topanroy-t5q" ><FaYoutube color='red'/></a>
+            <a href="https://www.facebook.com/TopanRoy41105/" ><FaFacebook  color='blue'/></a>
+            <a href="https://www.instagram.com/topan_roy41105/?igsh=dWdpYmUxcDNuZnhz#" ><FaSquareInstagram  color='red'/></a>
+            <a href="https://www.linkedin.com/in/topanroy/" ><FaLinkedin color='blue'/></a>
+            <a href="https://www.youtube.com/@Topanroy-t5q" ><FaYoutube color='red'/></a>
           </div>
         </div>
 
@@ -28,19 +27,8 @@ const Footer = () => {
           <ul className="space-y-1 text-sm">
             <li><a href="/" className="hover:text-indigo-500">Home</a></li>
             <li><a href="/assignments" className="hover:text-indigo-500">Assignments</a></li>
-            <li><a href="/create-assignment" className="hover:text-indigo-500">Create Assignment</a></li>
-            <li><a href="/my-submissions" className="hover:text-indigo-500">My Submissions</a></li>
-          </ul>
-        </div>
-
-        {/* Resources */}
-        <div>
-          <h3 className="text-lg font-semibold mb-2">Resources</h3>
-          <ul className="space-y-1 text-sm">
-            <li >FAQs</li>
-            <li>Support</li>
-            <li >Community Forum</li>
-            <li >Terms of Service</li>
+            <li className="hover:text-indigo-500">Create Assignment</li>
+            <li className="hover:text-indigo-500">My Submissions</li>
           </ul>
         </div>
 
